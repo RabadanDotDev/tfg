a42ac289dfd1d03610717eac71e80aee35147afe
Raul Rabadan Arroyo <raul@rabadan.dev>
2024-02-16 09:32:43 +0100

    update tlmgr before building container and install all deps in one command

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -2,16 +2,18 @@ apt-get update -y && apt-get upgrade -y
 wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.7_all.deb -P /tmp
 apt install -y /tmp/ttf-mscorefonts-installer_3.7_all.deb
 fc-cache -f -v
-tlmgr install fontspec
-tlmgr install xcolor
-tlmgr install babel-spanish
-tlmgr install babel-catalan
-tlmgr install xkeyval
-tlmgr install imakeidx
-tlmgr install tocloft
-tlmgr install etoolbox
-tlmgr install glossaries
-tlmgr install glossaries-spanish
-tlmgr install hyphen-spanish
-tlmgr install titlesec
-tlmgr install blindtext
+tlmgr update --self
+tlmgr install\
+    fontspec\
+    xcolor\
+    babel-spanish\
+    babel-catalan\
+    xkeyval\
+    imakeidx\
+    tocloft\
+    etoolbox\
+    glossaries\
+    glossaries-spanish\
+    hyphen-spanish\
+    titlesec\
+    blindtext
