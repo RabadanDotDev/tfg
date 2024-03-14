a126da2628548e39023d03f6702d1394ef05e2d7
Raul Rabadan Arroyo <raul@rabadan.dev>
2024-03-15 00:13:35 +0100

    add numbers and intervals informations

--- scripts/analize_cicddos_2019.py
+++ scripts/analize_cicddos_2019.py
@@ -1,4 +1,5 @@
 import glob
+from datetime import datetime
 import pandas
 
 def get_csvs_in_folder(folder: str):
@@ -6,6 +7,7 @@ def get_csvs_in_folder(folder: str):
 
 def analize_csv(file: str):
     df = pandas.read_csv(file, sep=',')
+    results = {}
 
     # Show general information
     print(f"{file} has {df.shape[0]} rows and {df.shape[1]} columns")
@@ -13,6 +15,9 @@ def analize_csv(file: str):
     for idx, na_num in enumerate(df.isna().sum()):
         if na_num != 0:
             print(f">>{df.columns[idx]} has {na_num} missing values")
+    results['first_csv_time'] = datetime.fromisoformat(df[' Timestamp'].min())
+    results['last_csv_time'] = datetime.fromisoformat(df[' Timestamp'].max())
+    print(f"Contains information from {results['first_csv_time']} to {results['last_csv_time']}")
 
     # Show ip information
     print(f">{df[[' Source IP']].drop_duplicates().shape[0]} unique origin ip addresses")
@@ -22,17 +27,28 @@ def analize_csv(file: str):
     # Show labeling information
     print(f">The dataset contains {df[[' Label']].drop_duplicates().shape[0]} labels")
     vc = df[' Label'].value_counts()
+    results['labels'] = {}
     for idx,number in enumerate(vc):
-        first_time = df.loc[df[' Label'] == vc.index[idx]][' Timestamp'].min()
-        last_time = df.loc[df[' Label'] == vc.index[idx]][' Timestamp'].max()
-        
-        print(f">>{vc.index[idx]} appears in {number} flows between {first_time} and {last_time}")
+        label = vc.index[idx]
+        results['labels'][label] = {"number": number}
+        results['labels'][label]['first_time'] = datetime.fromisoformat(df.loc[df[' Label'] == vc.index[idx]][' Timestamp'].min())
+        results['labels'][label]['last_time'] = datetime.fromisoformat(df.loc[df[' Label'] == vc.index[idx]][' Timestamp'].max())
+        print(f">>{label} appears in {number} flows between {results['labels'][label]['first_time']} and {results['labels'][label]['last_time']}")
+
+    print(f"{results}")
+    return results
+    
 
 def main():
+    results = {"03-11": {}, "01-12" : {}}
+
+    # Analize first file
     for file in get_csvs_in_folder("/Datasets/CICDDoS2019/csv/03-11/"):
-        analize_csv(file)
+        results["03-11"][file.split('/')[-1]] = analize_csv(file)
+
     for file in get_csvs_in_folder("/Datasets/CICDDoS2019/csv/01-12/"):
-        analize_csv(file)
+        results["01-12"][file.split('/')[-1]] = analize_csv(file)
+    print(f"{results}")
 
 if __name__ == "__main__":
     main()
