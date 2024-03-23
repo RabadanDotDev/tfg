1150690892d3755e4ab4ce413f009d51e09e8a0b
Raul Rabadan Arroyo <raul@rabadan.dev>
2024-03-23 18:37:57 +0000

    rename folders and files used to dump the data extracted with tshark

--- scripts/extract_info_cicddos_2019_pcaps_tshark.sh
+++ scripts/extract_info_cicddos_2019_pcaps_tshark.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 FOLDER_1="/Datasets/CICDDoS2019/pcap/03-11"
-FOLDER_1_RESULTS_FOLDER="./tmp/03-11.pcap"
+FOLDER_1_RESULTS_FOLDER="./tmp/cicddos2019-pcap-03-11"
 FOLDER_2="/Datasets/CICDDoS2019/pcap/01-12"
-FOLDER_2_RESULTS_FOLDER="./tmp/01-12.pcap"
+FOLDER_2_RESULTS_FOLDER="./tmp/cicddos2019-pcap-01-12"
 
 function analyze_pcap_folder {
     mkdir -p $2
-
 
     for pcap in $(find $1 -type f | sort -V)
     do
         echo "Generating conversations csv for $pcap"
         file=$(basename "$pcap")
-        file="$FOLDER_1_RESULTS_FOLDER/$file.csv"
+        file="$2/$file.conv.csv"
         echo "ip origin, ip dest, frames_to_origin, bytes_to_origin, frames_to_dest, bytes_to_dest, frames_total, bytes_total, relative_start, duration" > $file
         tshark -q -z conv,ip -r "$pcap" | tail -n +6 | head -n -1 | tr -d "<>\-," | tr -s " " | awk '{ print $1 ", " $2 ", " $3 ", " $4 " " $5 ", " $6 ", " $7 " " $8 ", " $9 ", " $10 " " $11 ", " $12 ", " $13}' >> $file
     done
@@ -21,7 +20,7 @@ function analyze_pcap_folder {
     for pcap in $(find $1 -type f | sort -V)
     do
         file=$(basename "$pcap")
-        file="$FOLDER_1_RESULTS_FOLDER/$file.treeproto.txt"
+        file="$2/$file.treeproto.txt"
         echo "Generating full tree protocols txt $pcap"
 
         tshark -q -z io,phs -r "$pcap" > $file
@@ -30,7 +29,7 @@ function analyze_pcap_folder {
     for pcap in $(find $1 -type f | sort -V)
     do
         file=$(basename "$pcap")
-        file="$FOLDER_1_RESULTS_FOLDER/$file.ipproto.csv"
+        file="$2/$file.ipproto.txt"
         echo "Generating stats ipv4 protocols protocols txt of $pcap"
 
         tshark -q -z ptype,tree -r "$pcap" > $file
