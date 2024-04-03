5081bb56fbc0828e21ee4fe80429cfec14306299
Raul Rabadan Arroyo <raul@rabadan.dev>
2024-04-03 18:57:09 +0000

    add pcap and etherparse as dependencies

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,8 +1,15 @@
+# Update
 apt-get update -y && apt-get upgrade -y
+
+# Install libpcap for tool
+apt install -y libpcap-dev
+
+# Install fonts for latex
 wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.7_all.deb -P /tmp
 apt install -y /tmp/ttf-mscorefonts-installer_3.7_all.deb
 fc-cache -f -v
 
+# Setup python dev env
 /usr/local/python/current/bin/python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
