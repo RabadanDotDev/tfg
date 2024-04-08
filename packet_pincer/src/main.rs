4abe261ea54e1e333fb52b37bcf4d5305222568d
Raul Rabadan Arroyo <raul@rabadan.dev>
2024-04-08 15:25:50 +0000

    parse args with offline/online command + dir/interface

--- packet_pincer/src/main.rs
+++ packet_pincer/src/main.rs
@@ -1,3 +1,39 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+use pcap::Device;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Settings {
+    #[command(subcommand)]
+    pub analysis: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    // Perform the analysis with a given set of network traces
+    OfflineAnalysis {
+        /// Sets the file or directory of network traces to analyze
+        #[arg(short, long, value_name = "FILE/DIRECTORY")]
+        traces_dir: PathBuf,
+    },
+    // Perform the analysis from captured network traffic
+    OnlineAnalysis {
+        /// Sets the network interface to capture traces from
+        #[arg(short, long, value_name = "INTERFACE")]
+        network_interface: Device,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let settings = Settings::parse();
+
+    match settings.analysis {
+        Commands::OfflineAnalysis { traces_dir } => {
+            todo!()
+        },
+        Commands::OnlineAnalysis { network_interface: _ } => {
+            todo!()
+        }
+    }
 }
