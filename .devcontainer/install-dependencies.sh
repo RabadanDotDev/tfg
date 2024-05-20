e12637c5a8893c26d8d69b5308e0c242dbb4ff88
Raul Rabadan Arroyo <raul@rabadan.dev>
2024-05-20 12:32:32 +0000

    intro packet pincer

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -10,3 +10,6 @@ fc-cache -f -v
 /usr/local/python/current/bin/python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+# Download a more recent verison of plantuml than the default repos
+wget https://github.com/plantuml/plantuml/releases/download/v1.2024.4/plantuml-1.2024.4.jar -O /opt/plantuml.jar
